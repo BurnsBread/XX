ab4add6bb6d8345d47ff4d266f1caa342b266e74
BurnsBread <141624048+BurnsBread@users.noreply.github.com>
2023-08-07 17:02:10 +0800

    Add files via upload

---
+++ 8.6/8.6/test.c
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+//int main()
+//{
+//	30;
+//	3.14;
+//	const int a = 10;
+//
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+
+//#define a "abcd"
+//
+//int main()
+//{
+//	printf("%s\n", a);
+//
+//	return 0;
+//}
+//
+
+//
+
+//
+
+//int main()
+//{
+//	int line = 0;
+//	printf("开始学习\n");
+//
+//	while (line < 2000)
+//	{
+//		line++;
+//		printf("写代码：%d\n", line);
+//	}
+//	if (line >= 2000)
+//	{
+//		printf("好offer\n");
+//	}
+//	else
+//	{
+//		printf("继续加油\n");
+//	}
+//
+//	return;
+//}
+
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//
+//	scanf("%d %d", &num1, &num2);
+//	int sum = Add(num1, num2);
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
+int main()
+{
+
+int arr[10] = { 10,11,12,13,14,15,16,17,18,19 };
+
+int i = 0;
+while(i < 10)
+{
+	printf("%d", arr[i]);
+	i = i + 1;
+}
+return 0;
+}
